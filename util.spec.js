89c3a4fbd86c07af91b04989555db51d22be3d55
Liz Zepeda <karla.zepeda@sprint.com>
2019-09-23 12:51:38 -0400

    e2e test

--- util.spec.js
+++ util.spec.js
@@ -1,6 +1,7 @@
 const { generateText, checkAndGenerate } = require('./util.js');
+const puppeteer = require('puppeteer');
 
-test ('should output name and age', () => {
+test('should output name and age', () => {
     // to test false positives 
     const text = generateText('Liz',29);
     expect(text).toBe('Liz (29 years old)');    
@@ -8,12 +9,31 @@ test ('should output name and age', () => {
     expect(text2).toBe('Diana (25 years old)');
 });
 
-test ('should output data-less', () => {
+test('should output data-less', () => {
     const text = generateText('',null);
     expect(text).toBe(' (null years old)');
 });
 
-test ('should validate and output name & age', () => {
+test('should validate and output name & age', () => {
     const text = checkAndGenerate('Liz',29);
     expect(text).toBe('Liz (29 years old)');
-})
+});
+
+test('should create object with text and correct class on e2e', async () => {
+    const browser =  await puppeteer.launch({
+        headless: false,
+        slowMo: 80,
+        args:['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto('file:///Users/lizzepeda/demos/js-testing/index.html');
+
+    await page.click('input#name');
+    await page.type('input#name','Liz');
+    await page.click('input#age');
+    await page.type('input#age','20');
+    await page.click('button#btnAddUser');
+
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Liz (20 years old)')
+}, 10000);
