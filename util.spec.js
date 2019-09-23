2d51d7853199082253b205668cf56fc76078cebd
Liz Zepeda <karla.zepeda@sprint.com>
2019-09-23 12:19:53 -0400

    simple unit test

---
+++ util.spec.js
@@ -0,0 +1,14 @@
+const { generateText } = require('./util.js');
+
+test ('should output name and age', () => {
+    // to test false positives 
+    const text = generateText('Liz',29);
+    expect(text).toBe('Liz (29 years old)');    
+    const text2 = generateText('Diana',25);
+    expect(text2).toBe('Diana (25 years old)');
+});
+
+test ('should output data-less', () => {
+    const text = generateText('',null);
+    expect(text).toBe(' (null years old)');
+})
